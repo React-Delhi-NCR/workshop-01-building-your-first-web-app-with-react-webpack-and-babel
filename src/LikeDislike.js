21a83dcb6b239d65637dcaf5c3bb2e4c149825a8
Arun Michael Dsouza <amdsouza92@gmail.com>
2019-03-26 23:11:16 +0530

    LikeDislike component added

---
+++ src/LikeDislike.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const EMOTINOS = {
+  "-1": " 👎",
+  "0": "🤗",
+  "1": "👍"
+};
+
+class LikeDislike extends React.Component {
+  state = {
+    emotion: 0
+  };
+
+  changeEmotion = emotion => {
+    this.setState({ emotion });
+  };
+
+  render() {
+    const { emotion } = this.state;
+
+    return (
+      <div>
+        <h3>What you'r reaction to above blog {EMOTINOS[emotion]}</h3>
+        <div>
+          <button onClick={() => this.changeEmotion(1)}>Like</button>{" "}
+          <button onClick={() => this.changeEmotion(-1)}>DisLike</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LikeDislike;
